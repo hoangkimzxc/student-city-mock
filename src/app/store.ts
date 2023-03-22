8c6caed1d2538270ce0b0ec4d352b53ea633b548
hoangnk <hoangkimzxc@gmail.com>
2023-03-22 10:57:30 +0700

    P3

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./rootSaga";
 
+const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 export type AppDispatach = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
