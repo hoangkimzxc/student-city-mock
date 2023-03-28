20d0378d8eb604ab72f5287385ab4852ba112c7a
hoangnk <hoangkimzxc@gmail.com>
2023-03-28 10:56:47 +0700

    P10

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,23 +1,42 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { fork, take } from "redux-saga/effects";
+import { call, delay, fork, put, take } from "redux-saga/effects";
 import { authActions, LoginPayload } from "./authSlice";
 
 function* handleLogin(payload: LoginPayload) {
-  console.log("Handle Login", payload);
+  try {
+    console.log("Handle Login", payload);
+    yield delay(1000);
+    localStorage.setItem("access_token", "fake_token");
+    yield put(
+      authActions.loginSuccess({
+        id: 1,
+        name: "Easy Frontend",
+      })
+    );
+  } catch (error: any) {
+    yield put(authActions.loginFailed(error.message));
+  }
 }
 
 function* handleLogout() {
+  yield delay(500);
   console.log("Handle Logout");
+  localStorage.removeItem("access_token");
+  //redirect to login page
 }
 
 function* watchLoginFlow() {
   while (true) {
-    const action: PayloadAction<LoginPayload> = yield take(
-      authActions.login.type
-    );
-    yield fork(handleLogin, action.payload);
+    console.log("watch login");
+    const isLoggedIn = Boolean(localStorage.getItem("access_token"));
+    if (!isLoggedIn) {
+      const action: PayloadAction<LoginPayload> = yield take(
+        authActions.login.type
+      );
+      yield fork(handleLogin, action.payload);
+    }
     yield take(authActions.logout.type);
-    yield fork(handleLogout);
+    yield call(handleLogout);
   }
 }
 
