eedb51ed0bc4cccfb1ff5734efd43ecaa7afc62d
hoangnk <hoangkimzxc@gmail.com>
2023-05-08 11:08:40 +0700

    P40

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,30 @@ import StudentFeature from "./features/student";
 import ListPage from "./features/student/pages/ListPage";
 import AddEditPage from "./features/student/pages/AddEditPage";
 import { useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export default function App() {
+  // Import the functions you need from the SDKs you need
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBGhDRqXM0Jziitwo6sXU7-gpU67oChSSw",
+    authDomain: "student-city-mock-c89c8.firebaseapp.com",
+    projectId: "student-city-mock-c89c8",
+    storageBucket: "student-city-mock-c89c8.appspot.com",
+    messagingSenderId: "291427195794",
+    appId: "1:291427195794:web:2d263844cdc952c1487f4b",
+    measurementId: "G-0X5CT6ZRNM",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
   const navigate = useNavigate();
   useEffect(() => {
     const token = localStorage.getItem("access_token");
